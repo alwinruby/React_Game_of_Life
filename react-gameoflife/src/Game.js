64aa4364030f45c90b16e82394af4732a0a5edc4
asolanky <alwinsolanky@gmail.com>
2019-05-03 08:56:34 +0100

    amended Game.css 2

--- react-gameoflife/src/Game.js
+++ react-gameoflife/src/Game.js
@@ -8,7 +8,8 @@ class Game extends React.Component {
     return (
       <div>
         <div className="Board"
-          style={{ width: WIDTH, height: HEIGHT }}>
+          style={{ width: WIDTH, height: HEIGHT,
+            backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}>
         </div>
       </div>
     );
